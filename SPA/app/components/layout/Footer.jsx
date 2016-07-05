c0d6498d2f95c36c1169c82489105da04c3618ee
sergioconsuelo <sergio11ortiz@gmail.com>
2016-07-05 13:38:04 -0500

    responsive view for login

--- SPA/app/components/layout/Footer.jsx
+++ SPA/app/components/layout/Footer.jsx
@@ -1,13 +1,38 @@
 import React from 'react';
 
 class Footer extends React.Component {
-   render() {
-      return (
-         <div>
-            Footer
-         </div>
-      );
-   }
+    render() {
+        var mainContainer = {
+            minHeight: '100%',
+            height: 'auto !important',
+            height: '100%',
+            margin: '0 auto -35px'
+        };
+        return (
+            <div className="footer" id="footerLog">
+                <div className="container-fluid">
+                    <div className="row text-xs-center">
+                        <div className="col-sm-5 col-xl-3">
+                            <p>&copy; 2016 Anzen Soluciones S.A. de C.V.</p>
+                        </div>
+                        <div className="col-sm-7 col-xl-4 col-xl-offset-5">
+                            <p>
+                                <a href="#">Aviso legal</a> |
+                                <a href="#">&nbsp;Sucursales</a> |
+                                <a href="#">&nbsp;Cont&aacute;ctanos</a> |
+                                <a href="#">&nbsp;Ayuda</a>
+                                <select className="form-control">
+                                    <option>Lenguaje</option>
+                                    <option>Español</option>
+                                    <option>Ingl&eacute;s</option>
+                                </select>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Footer;
--- SPA/app/components/login/Login.jsx
+++ SPA/app/components/login/Login.jsx
@@ -1,13 +1,88 @@
 import React from 'react';
 
 class Login extends React.Component {
-   render() {
-      return (
-         <div>
-            Login
-         </div>
-      );
-   }
+    render() {
+        var mainContainer = {
+            minHeight: '100%',
+            height: 'auto !important',
+            height: '100%',
+            margin: '0 auto -35px'
+        };
+        return (
+            <div>
+                <div className="dark-line">
+                    <div className="line-head"></div>
+                </div>
+                
+                <div className="container" style={ mainContainer }>
+                    <br></br>
+                    
+                    <div className="row">
+                        <div className="col-lg-10 col-lg-offset-1 col-xl-8 col-xl-offset-2">
+                            <div className="card text-xs-center shadow">
+
+                                <div className="card-block" id="images">
+                                    <div className="logo-login"></div>
+                                    <div className="row">
+                                        <div className="col-xs-12">
+                                            <h5 className="card-title">
+                                                Bienvenido<br></br>
+                                                <small>Sergio Xavier Consuelo Ortiz</small>
+                                            </h5>
+                                            <p>
+                                                <small>
+                                                    <span>Nivel&nbsp;de&nbsp;seguridad&nbsp;adicional. </span>
+                                                    Selecciona&nbsp;la&nbsp;imagen&nbsp;que&nbsp;elegiste&nbsp;al&nbsp;registrarte.
+                                                </small>
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div className="container-fluid">
+                                        <div className="row">
+                                            <div className="col-xs-12 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2">
+                                                <div className="img1_opt"></div>
+                                                <div className="img2_opt"></div>
+                                                <div className="img3_opt"></div>
+                                                <div className="img4_opt"></div>
+                                                <div className="img5_opt"></div>
+                                                <div className="img6_opt"></div>
+                                                <div className="img7_opt"></div>
+                                                <div className="img8_opt"></div>
+                                                <div className="img9_opt"></div>
+                                                <div className="img10_opt"></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="row">
+                                        <div className="col-md-8 col-md-offset-2 col-xl-6 col-xl-offset-3">
+                                            <input type="password" className="form-control" placeholder="Contraseña"></input>
+                                        </div>
+                                    </div>
+                                    <div className="row">
+                                        <div className="space col-md-4 col-md-offset-2 col-xl-3 col-xl-offset-3">
+                                            <a href="#" className="btn btn-danger btn-block">Cancelar</a>
+                                        </div>
+                                        <div className="space col-md-4 col-xl-3">
+                                            <a href="#" className="btn btn-primary btn-block">Continuar</a>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="card-footer">
+                                    <p>
+                                        <small className="text-muted">Comunícate con nosotros al 01 800 800 32 24</small>
+                                    </p>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <br></br><br></br>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Login;
